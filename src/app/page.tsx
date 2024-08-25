77becb288cb83ecf8a5b6e9d4a8c583319c0f363
Joy Brar <joybrar2001@gmail.com>
2024-08-26 00:32:34 +0530

    Completed Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
+import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
+      <ProjectsSection />
+      <TapeSection />  
     </>
   );
 }
